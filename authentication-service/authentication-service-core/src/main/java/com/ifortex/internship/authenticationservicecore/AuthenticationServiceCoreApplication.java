c81aa28860972ccb54f55d451b5820c1a329e694
Vladislav Protchenko <vladislav.protchenko@ifortex.com>
2024-12-02 01:08:37 +0300

    created authentication-service-core module

---
+++ authentication-service/authentication-service-core/src/main/java/com/ifortex/internship/authenticationservicecore/AuthenticationServiceCoreApplication.java
@@ -0,0 +1,13 @@
+package com.ifortex.internship.authenticationservicecore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationServiceCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationServiceCoreApplication.class, args);
+	}
+
+}
---
+++ authentication-service/authentication-service-core/src/test/java/com/ifortex/internship/authenticationservicecore/AuthenticationServiceCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifortex.internship.authenticationservicecore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationServiceCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
